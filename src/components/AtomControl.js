56a1bd44b13c5c10eccc44883bde20d03a4c5d77
MRECoelho <mrecoelho@gmail.com>
2021-01-09 16:03:41 +0100

    Created AtomControl with its css

---
+++ src/components/AtomControl.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AtomControl = () => {
+    return (
+        <span className="atomcontrol">
+            &bull;
+        </span>
+    )
+}
+
+export default AtomControl
--- src/components/List.js
+++ src/components/List.js
@@ -9,6 +9,7 @@ import {store} from '../store/store'
 import { useDispatch,  useSelector } from 'react-redux'
 import thunk from 'redux-thunk';
 import TextareaAutosize from 'react-textarea-autosize'
+import AtomControl from './AtomControl'
 // import { useDispatch } from 'react-redux'
 
 export default function List() {
@@ -44,34 +45,38 @@ export default function List() {
                     if(atom.hidden === false){
                         return(
                             <li key={atom.id}>
-                                <div className="atomContentContainer" style={{marginLeft:atom.indent*35 + 15/atom.indent}}>
-                                    <TextareaAutosize
-                                        name="title"
-                                        className="textarea textarea-title"
-                                        value={atom.title}
-                                        minRows={1}
-                                        spellCheck="false"
-                                        // onChange={onChangeTitleEvent}
-                                        onFocus={e => onFocusWrapper(atom.id, e)}
-                                        onBlur={e => onBlurWrapper(atom.id, e)}
-                                        
-                                        // ref={ref}
-                                    ></TextareaAutosize>
-                                    {atom.notes?
-                                    <TextareaAutosize
-                                        name="notes"
-                                        className="textarea textarea-notes"
-                                        value={atom.notes}
-                                        minRows={1}
-                                        spellCheck="false"
-                                        // onChange={onChangeTitleEvent}
-                                        onFocus={e => onFocusWrapper(atom.id, e)}
-                                        onBlur={e => onBlurWrapper(atom.id, e)}
-                                        
-                                        // ref={ref}
-                                    ></TextareaAutosize> :
-                                    null
-                                    }
+                                <div className="atomContainer" style={{marginLeft:atom.indent*35 + 15/atom.indent}}>
+                                    <AtomControl></AtomControl>
+                                    <div className="atomContentContainer">
+                                        <TextareaAutosize
+                                            name="title"
+                                            className="textarea textarea-title"
+                                            value={atom.title}
+                                            minRows={1}
+                                            spellCheck="false"
+                                            // onChange={onChangeTitleEvent}
+                                            onFocus={e => onFocusWrapper(atom.id, e)}
+                                            onBlur={e => onBlurWrapper(atom.id, e)}
+                                            
+                                            // ref={ref}
+                                        ></TextareaAutosize>
+                                        {atom.notes?
+                                        <TextareaAutosize
+                                            name="notes"
+                                            className="textarea textarea-notes"
+                                            value={atom.notes}
+                                            minRows={1}
+                                            spellCheck="false"
+                                            // onChange={onChangeTitleEvent}
+                                            onFocus={e => onFocusWrapper(atom.id, e)}
+                                            onBlur={e => onBlurWrapper(atom.id, e)}
+                                            
+                                            // ref={ref}
+                                        ></TextareaAutosize> :
+                                        null
+                                        }
+
+                                    </div>
                                     {/* <div className="atomContentTitle">{atom.title}</div> */}
                                     {/* <div className="atomContentNotes">{atom.notes}</div> */}
                                 </div>
